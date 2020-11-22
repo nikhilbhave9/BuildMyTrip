0926a45d0be45e6db3a9f19cd9116e8d85f0b21d
XSiege999 <nik.3731@gmail.com>
2020-11-22 22:45:31 +0530

    Re-added hotel schema and models

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,10 @@ connection.once('open', () => {
 });
 
 const usersRouter = require('./routes/users');
+const hotelsRouter = require('./routes/hotels');
 
 app.use('/users', usersRouter);
+app.use('/hotels', hotelsRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
