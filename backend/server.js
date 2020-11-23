4aa124bd281afe55a74c7d2d618b29ef320277f7
XSiege999 <nik.3731@gmail.com>
2020-11-23 11:47:16 +0530

    Added documentation

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const cors = require('cors');  // Used to allow OR restrict access depending on where HTTP request was initiated 
+const mongoose = require('mongoose');  // Better workflow with MongoDB
 
 require('dotenv').config();
 
@@ -10,12 +10,18 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// ==========================================================================
+// Connect Node server to MongoDB database via MongoDB Atlas
+
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true});
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 });
+
+// ==========================================================================
+// Add routes 
 
 const usersRouter = require('./routes/users');
 const hotelsRouter = require('./routes/hotels');
