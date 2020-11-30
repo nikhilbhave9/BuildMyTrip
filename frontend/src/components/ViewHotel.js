7485fe465d4beea57f97f8c82549df6388c80221
Akshat-Singh <akshatsingh.hs@gmail.com>
2020-11-30 16:40:44 +0530

    Changed Homepage

--- frontend/src/components/ViewHotel.js
+++ frontend/src/components/ViewHotel.js
@@ -106,7 +106,7 @@ export default class ViewHotel extends Component {
                                     }
                                     }>
                                     Book a room
-                        </Button>
+                                </Button>
                             </Link>
                             <Link>
                                 <Button
@@ -121,7 +121,7 @@ export default class ViewHotel extends Component {
                                     }
                                     }>
                                     Add to tracker
-                        </Button>
+                                </Button>
                             </Link>
                         </div>
                         <div className="right">
@@ -243,9 +243,9 @@ export default class ViewHotel extends Component {
                                                         return (
                                                             <Slide index={index}>
                                                                 <div style={{ fontFamily: 'satisfy', fontSize: '20px' }}>
-                                                                    <Link 
-                                                                        style={{fontSize: '20px'}}
-                                                                        to={'' + item._id}>{item.itemName}</Link>:  
+                                                                    <Link
+                                                                        style={{ fontSize: '20px' }}
+                                                                        to={'' + item._id}>{item.itemName}</Link>:
                                                                     ({item.location}) {item.standardRating}
                                                                     <BsIcons.BsStarFill style={{ color: 'yellow' }} />/5
                                                                 </div>
@@ -267,21 +267,21 @@ export default class ViewHotel extends Component {
                                                                         )
                                                                     })}
                                                                 </p>
-                                                                <div 
-                                                                    style={{ 
-                                                                        fontFamily: 'ubuntu', 
-                                                                        marginTop: '5px' 
+                                                                <div
+                                                                    style={{
+                                                                        fontFamily: 'ubuntu',
+                                                                        marginTop: '5px'
                                                                     }}
                                                                 >
                                                                     Average User Rating: {item.userRating}<BsIcons.BsStarFill style={{ color: 'yellow' }} />/5
-                                                                    Evaluated from {item.ratings.length + 1} reviews. 
+                                                                    Evaluated from {item.ratings.length + 1} reviews.
                                                                 </div>
 
                                                             </Slide>)
                                                     })}
                                                 </Slider>
-                                                                <ButtonBack>&#60;</ButtonBack>
-                                                                <ButtonNext>&#62;</ButtonNext>
+                                                <ButtonBack>&#60;</ButtonBack>
+                                                <ButtonNext>&#62;</ButtonNext>
                                             </CarouselProvider>
 
                                         </td>
@@ -291,7 +291,7 @@ export default class ViewHotel extends Component {
 
                         </div>
                     </div>
-                        </div>
+                </div>
             </>
         )
     }
